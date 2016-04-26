75a1a35d0f712aca5c3e3660f1448c58cc12b4dc
Evandro Myller <eMyller@7ws.co>
2016-04-26 00:59:38 -0300

    Set up Redis to store data

--- backend/settings.py
+++ backend/settings.py
@@ -1,13 +1,16 @@
 from unipath import Path
+from decouple import config as read_conf  # `config` clashes with Muffin
+
+from backend.url_parser import parse_url
 
 
 # Basic (self-explanatory) definitions
 BASE_DIR = Path(__file__).ancestor(2)
 
-
 # Muffin plug-ins
 PLUGINS = [
     'muffin_jade',
+    'muffin_redis',
 ]
 
 # Important directories
@@ -18,3 +21,10 @@ STATIC_FOLDERS = [
 JADE_TEMPLATE_FOLDERS = [
     BASE_DIR.child('frontend', 'templates'),
 ]
+
+# Redis database
+REDIS_URL = read_conf('REDIS_URL', 'redis://localhost/0', parse_url)
+REDIS_HOST = REDIS_URL['hostname']
+REDIS_PORT = REDIS_URL['port']
+REDIS_PASSWORD = REDIS_URL['password']
+REDIS_DB = int(REDIS_URL['path'][1:])
