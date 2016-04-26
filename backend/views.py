68b0901f7fbb4d53b596dedfd1401a1d6a6f4b4d
Evandro Myller <eMyller@7ws.co>
2016-04-26 20:55:59 -0300

    Introduce a simple websocket view

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,22 @@
+from aiohttp import web
+import aiohttp
+
 from shatup import app
 
 
 @app.register('/')
 def main(request):
     return app.ps.jade.render('main.jade')
+
+
+@app.register('/chat.ws')
+async def chat_websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    async for message in ws:
+        if message.tp == aiohttp.MsgType.text:
+            if message.data == 'close':
+                await ws.close()
+            else:
+                ws.send_str(message.data)
+    return ws
